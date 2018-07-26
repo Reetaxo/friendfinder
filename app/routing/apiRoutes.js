96cd2d04f76146a1d7c241b6c5ac7633ac0fcd04
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-07-26 00:46:35 -0400

    more

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
 var friends = require("../data/friends");
 
 module.exports - function(app) {
-app.get("/api/answers", function(req, res){
+app.get("/api/survey", function(req, res){
     res.JSON(friends);
 });
 
-app.post("/api/answers", function(req, res){
-
+app.post("/api/survey", function(req, res){
+res.send(req.body);
 
 })
 
--- server.js
+++ server.js
@@ -9,8 +9,10 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
+
+
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 
 app.listen(PORT, function() {
