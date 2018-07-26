cc7b84bdb85e16fd8abc32d9dd936c8ae89f96f9
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-07-25 20:34:05 -0400

    more

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,13 @@
+var friends = require("../data/friends");
 
+module.exports - function(app) {
+app.get("/api/answers", function(req, res){
+    res.JSON(friends);
+});
+
+app.post("/api/answers", function(req, res){
+
+
+})
+
+}
