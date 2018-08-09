e6ab41b67bfdf8b0b7e97469b40d955b7ee79657
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-08-09 09:57:05 -0400

    more

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,17 @@
 var friends = require("../data/friends");
 
-module.exports - function(app) {
-app.get("/api/survey", function(req, res){
-    res.JSON(friends);
+module.exports = function(app) {
+    app.get("/api/answers/", function(req, res){
+        console.log("hello");
+        res.json(friends);
+        
+    });
+
+    app.post("/api/survey", function(req, res){
+    res.send(req.body);
+
 });
 
-app.post("/api/survey", function(req, res){
-res.send(req.body);
-
-})
-
 }
+// comparing all users using absolute value
+// store users and compare other users
